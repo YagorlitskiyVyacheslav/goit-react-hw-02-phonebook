ca41bf2038273727c100c29d37b39c574d54fcc6
Vyacheslav <slavasan05@gmail.com>
2020-10-06 17:54:53 +0200

    Updates

---
+++ precache-manifest.1fd55a12a1100b62dbd75d084139e4fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930ad1f5e53781f3979feb201a6d13c9",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "c6a2930b53bb0008f821",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.055e379e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.055e379e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd09298372421a4d3b3",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.009302a4.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
